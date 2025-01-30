7d1607dabd1288010004c613ceb409dc012a095c
Ian Goodwin <Ian.goodwin@kellogg.northwestern.edu>
2025-01-30 21:15:37 +0000

    additional testing

---
+++ testing_ruby.rb
@@ -0,0 +1,12 @@
+grocery_shopping = [
+    {"from" => nil,"to" => "store", "item" => "apple", "quantity" => 20}
+    {"from" => nil,"to" => "store", "item" => "banana", "quantity" => 20}
+    {"from" => nil,"to" => "store", "item" => "oreos", "quantity" => 20}
+    {"from" => nil,"to" => "store", "item" => "eggs", "quantity" => 20}
+    {"from" => "store","to" => "cart", "item" => "apple", "quantity" => 5}
+    {"from" => "store", "to" => "cart", "item" => "banana", "quantity" => 8}
+    {"from" => "store", "to" => "cart", "item" => "oreos", "quantity" => 2}
+    {"from" => "store", "to" => "cart", "item" => "eggs", "quantity" => 3}
+    {"from" => "cart", "to" => "store", "item" => "oreos", "quantity" => 1}
+]
+
