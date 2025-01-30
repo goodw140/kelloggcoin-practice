cfb5679a08e8131255cb0ca8b1e24929f988b77b
Ian Goodwin <Ian.goodwin@kellogg.northwestern.edu>
2025-01-30 17:25:11 +0000

    First try

--- practice.rb
+++ practice.rb
@@ -27,3 +27,64 @@ blockchain = [
 # Anthony's KelloggCoin balance is 2650
 
 # 👇👇👇 Your code HERE 👇👇👇
+
+# p blockchain
+
+# code below is to create accounts that can be added to for each of the transactions 
+# listed in the blockhain array
+ben_balance = 0
+brian_balance = 0
+evan_balance = 0
+anthony_balance = 0
+
+
+
+# code below is a loop to walk through each transaction
+for transaction in blockchain
+  
+  if transaction["to_user"] == "ben"
+    ben_balance = ben_balance + transaction["amount"]
+  elsif transaction["from_user"] == "ben"
+    ben_balance = ben_balance - transaction["amount"]
+  end
+  
+  if transaction["to_user"] == "brian"
+    brian_balance = brian_balance + transaction["amount"]
+  elsif transaction["from_user"] == "brian"
+    brian_balance = brian_balance - transaction["amount"]
+  end
+  
+  if transaction["to_user"] == "evan"
+    evan_balance = evan_balance + transaction["amount"]
+  elsif transaction["from_user"] == "evan"
+    evan_balance = evan_balance - transaction["amount"]
+  end
+
+  if transaction["to_user"] == "anthony"
+    anthony_balance = anthony_balance + transaction["amount"]
+  elsif transaction["from_user"] == "anthony"
+    anthony_balance = anthony_balance - transaction["amount"]
+  end
+
+  puts "Transaction from #{transaction["from_user"]} to #{transaction["to_user"]}: #{transaction["amount"]}"
+
+  puts nil
+  puts "Ben's current KelloggCoin balance is #{ben_balance}"
+  puts "Brian's current KelloggCoin balance is #{brian_balance}"
+  puts "Evan's current KelloggCoin balance is #{evan_balance}"
+  puts "Anthony's current KelloggCoin balance is #{anthony_balance}"
+  puts nil
+
+end
+
+
+# code below is final balances:
+puts nil
+puts "After all of the transactions:"
+puts nil
+puts "Ben's ending KelloggCoin balance is #{ben_balance}"
+puts "Brian's ending KelloggCoin balance is #{brian_balance}"
+puts "Evan's ending KelloggCoin balance is #{evan_balance}"
+puts "Anthony's ending KelloggCoin balance is #{anthony_balance}"
+puts nil
+puts "Total KelloggCoin in circulation is #{ben_balance + brian_balance + evan_balance + anthony_balance}"
